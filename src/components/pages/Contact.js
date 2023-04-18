5cb1172a695802d3759d9dc5c2690dea142890ef
WloMac <wlodzimierz.maciaszczyk@gmail.com>
2023-04-19 00:05:28 +0100

    adding emailjs to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 // import { Link, Route, Routes } from 'react-router-dom';
 // import Learn from './Learn';
 import './Contact.css'
 
 function Contact() {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_pcpyuwa', 'template_twmvbra', form.current, 'FhlLJ06ZMP2-MuzuI')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
   return (
     <section className='contactSection'>
       <h1>Contact Page</h1>
       <hr></hr>
-      <p> Email: wmac.webdev@gmail.com </p>
-        <p>LinkedIn:</p>
-        <p>GitHub:  </p>
+      <div className='formContainer'>
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
       {/* <Link to="learn" role="button" className="btn btn-link">
         Learn More
       </Link>
